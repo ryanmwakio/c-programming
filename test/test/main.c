47c60d4743e94fa5a1cb2cc656fec3d43bbaf3c1
Brian Ochieng <Bryern254@gmail.com>
2021-10-03 15:48:39 +0300

    push

--- test/test/main.c
+++ test/test/main.c
@@ -3,6 +3,16 @@
 
 int main()
 {
-    printf("");
+    char hello[] = "hello world";
+    int dayWorking = 1;
+    printf("first c programme %s %d \n\n", hello, dayWorking);
+
+    char name;
+
+    printf("input name here: ");
+    scanf("\n%s",&name);
+
+    printf("Your name is %s",name);
+
     return 0;
 }
