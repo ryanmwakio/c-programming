a06bb3e68cd7f891044dee5d36cb3f55cb4abda3
Brian Ochieng <Bryern254@gmail.com>
2021-10-04 22:21:42 +0300

    first class

--- Set-one/1) b)BeerBrandsAndPricesSwitchCase/main.c
+++ Set-one/1) b)BeerBrandsAndPricesSwitchCase/main.c
@@ -7,7 +7,7 @@ int main()
     printf("\n\t ****Jamal and DaughtersPub****\n\n");
     printf("\tBeer Brand \t\tPrice\n");
     printf("\t 1)Tusker\t\t100/=\n");
-    printf("\t 2)Plisner\t\t120/=\n");
+    printf("\t 2)Pilsner\t\t120/=\n");
     printf("\t 3)Smirnoff Ice\t\t140/=\n");
     printf("\t 4)White Cap\t\t90/=\n\n");
 
@@ -27,9 +27,9 @@ int main()
         printf("\n\t%d bottles of Tusker will cost you Kshs.%d\n",amount,amount*100);
         break;
     case 2:
-        printf("\n\tHow many bottles of Tusker do you want? ");
+        printf("\n\tHow many bottles of Pilsner do you want? ");
         scanf("%d",&amount);
-        printf("\n\t%d bottles of Tusker will cost you Kshs.%d\n",amount,amount*100);
+        printf("\n\t%d bottles of Pilsner will cost you Kshs.%d\n",amount,amount*100);
         break;
     case 3:
         printf("\n\tHow many bottles of Smirnoff Ice do you want? ");
--- test/test/main.c
+++ test/test/main.c
@@ -3,16 +3,15 @@
 
 int main()
 {
-    char hello[] = "hello world";
-    int dayWorking = 1;
-    printf("first c programme %s %d \n\n", hello, dayWorking);
-
     char name;
 
     printf("input name here: ");
     scanf("\n%s",&name);
+    printf("Your name is %s",&name);
 
-    printf("Your name is %s",name);
+    int number = 20;
+    char string = "A";
+    double decimal = 3.0;
 
     return 0;
 }
