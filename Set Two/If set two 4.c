5212d81aa52f79c5c85576227e94a6658a548ee0
Brian Ochieng <Bryern254@gmail.com>
2021-10-14 14:59:32 +0300

    what is up

---
+++ Set Two/If set two 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float x, y;
+	
+	printf("Enter the value of x: ");
+	scanf("%f",&x);
+	
+	if (X>5){
+		4X
+	}
+}
---
+++ Set Two/if set two 5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int marks;
+	
+	printf("Enter the marks_scored in five subjects: ");
+	scanf("%d",&marks_scored in five subjects);
+	
+	
+	
+	switch (marks)
+	{
+		case 1:(marks>75 && marks<100)
+		
+	}
+}
