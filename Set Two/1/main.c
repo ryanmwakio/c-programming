62e790ee9fd4720126ed96a1b0ef72296ca1c58c
Brian Ochieng <Bryern254@gmail.com>
2021-10-15 13:29:50 +0300

    moving to 11

--- Set Two/1/main.c
+++ Set Two/1/main.c
@@ -3,6 +3,17 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int number, remainder;
+
+    // Input
+    printf("\nInput an integer: ");
+    scanf("%d", &number);
+
+    remainder=number%2;
+    if (remainder == 0)
+        printf("\n\tNumber is even");
+    else
+        printf("\n\tNumber is odd\n");
+
     return 0;
 }
---
+++ Set Two/2/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+int main ()
+{
+
+	float gross_salary, net_salary, tax_amount;
+
+	printf("Enter the gross_salary: ");
+	scanf("%f",&gross_salary);
+
+	if(gross_salary>20000){
+        tax_amount = gross_salary * 15/100 ;
+        printf("\nThe tax_amount is %f\n",tax_amount);
+	}else if(gross_salary>=10000 && gross_salary<=20000){
+		printf ("\nThe tax_amount is %f\n\n",tax_amount,gross_salary * 10 % );
+	}else if(gross_salary<10000){
+        printf("\nNot taxed\n\n");
+	}
+
+    return 0;
+}
