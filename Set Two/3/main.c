711306b16109f9cc561d203de886e3e5c191b0c0
Brian Ochieng <bryern254@gmail.com>
2021-10-17 15:46:43 +0300

    what is question 3 🤦🏾‍♂️

--- Set Two/3/main.c
+++ Set Two/3/main.c
@@ -1,8 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<conio.h>
 
-int main()
+void main( )
 {
-    printf("Hello world!\n");
-    return 0;
+    int earning;
+    char gender = "m";
+    int genderTwo;
+
+    // Input
+    printf("\nEnter Gender(m/f): ");
+    scanf("%c", &gender);
+    printf("\nEnter your salary: ");
+    scanf("%d", &earning);
+
+    // due tolimits of c 🤦🏾‍♂️
+    if (gender = "f")
+        genderTwo = 0;
+    else
+        genderTwo = 1;
+
+    // Computation
+    if ((genderTwo==0) && (earning<15000))
+        printf("\n\tYour tax rate is 12 percent.\n");
+    else if ((genderTwo==0) && (earning>=15000))
+        printf("\n\tYour tax rate is 14 percent.\n");
+    else if (!(genderTwo==0) && (earning<14000))
+        printf("\n\tYour tax rate is 13 percent.\n");
+    else if (!(genderTwo==0) && (earning>=14000))
+        printf("\n\tYour tax rate is 15 percent.\n");
+    else
+        printf("\nWrong Data\n");
+
 }
