b28ba8caaf69f020a30a28453b1a9c6b07f60c03
Brian Ochieng <bryern254@gmail.com>
2021-10-20 16:45:36 +0300

    It works

--- Loops1/2_Sum_of_integers.c
+++ Loops1/2_Sum_of_integers.c
@@ -2,19 +2,10 @@
 
 void main()
 {
+    char end = "x";
     int num, sum;
 
     printf("\nProgram Will terminate when X is entered\nEnter a number:\n");
-
-    for(num = 1; num != "x"; sum += num)
-    {
-        printf("\t");
-        scanf("%d", &num);
-
-        if(num == "0"){
-            continue;
-        }
-    }
 
     printf("The sum is %d", sum);
 }
